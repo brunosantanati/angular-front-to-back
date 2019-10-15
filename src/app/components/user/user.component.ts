e729f918107c7b00103f7ead2364197b4d2689d8
Bruno Sant Ana <bruno.santana.ti@gmail.com>
2019-10-14 23:50:09 -0300

    Created an example component manually

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-user',
+    templateUrl: './user.component.html',
+    //template: '<h2>Bruno Sant Ana</h2>',
+    styleUrls: ['./user.component.css'],
+    styles: [`
+        h2{
+            font-family: verdana;
+        }
+    `]
+})
+
+export class UserComponent {
+
+}
