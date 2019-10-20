7d641782ae866fe9e859478c372b37b23bbffab3
Bruno Rafael Sant' Ana <bruno.fullstackdeveloper@gmail.com>
2019-10-20 07:14:53 -0300

    Add ngIf in the view. Add Bootstrap, jQuery and Popper. Fix layout.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
-import { UsersComponent } from './components/users/users.component'
+import { UsersComponent } from './components/users/users.component';
+import { NavbarComponent } from './components/navbar/navbar.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    UsersComponent
+    UsersComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
