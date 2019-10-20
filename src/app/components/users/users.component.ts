43f7c4856f1cf4c3df2a9f201d741ada3b8969e2
Bruno Rafael Sant' Ana <bruno.fullstackdeveloper@gmail.com>
2019-10-20 07:14:53 -0300

    Add Bootstrap, jQuery and Popper. Fix layout.

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -10,53 +10,56 @@ import { User } from '../../models/User'
 export class UsersComponent implements OnInit {
 
   users: User[];
+  showExtended: boolean = true;
+  loaded: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
-    this.users = [
-      {
-        firstName: 'Bruno',
-        lastName: 'Sant\' Ana',
-        age: 33,
-        address: {
-            street: '50 Main st',
-            city: 'Boston',
-            state: 'MA'
-        }
-      },
-      {
-        firstName: 'Kevin',
-        lastName: 'Johnson',
-        age: 34,
-        address: {
-            street: '20 School st',
-            city: 'Lynn',
-            state: 'MA'
-        }
-      },
-      {
-        firstName: 'Karen',
-        lastName: 'Williams',
-        age: 26,
-        address: {
-            street: '55 Mill st',
-            city: 'Miami',
-            state: 'FL'
-        }
-      }
-    ];
 
-    this.addUser({
-      firstName: 'David',
-      lastName: 'Jackson',
-      age: 44,
-      address: {
-          street: '12 Wake st',
-          city: 'Miami',
-          state: 'FL'
-      }
-    });
+    setTimeout(() => {
+      this.users = [
+        {
+          firstName: 'Bruno',
+          lastName: 'Sant\' Ana',
+          age: 33,
+          address: {
+              street: '50 Main st',
+              city: 'Boston',
+              state: 'MA'
+          }
+        },
+        {
+          firstName: 'Kevin',
+          lastName: 'Johnson',
+          age: 34,
+          address: {
+              street: '20 School st',
+              city: 'Lynn',
+              state: 'MA'
+          }
+        },
+        {
+          firstName: 'Karen',
+          lastName: 'Williams',
+          age: 26,
+          address: {
+              street: '55 Mill st',
+              city: 'Miami',
+              state: 'FL'
+          }
+        }
+      ];
+
+      this.loaded = true;
+
+      //this.showExtended = false;
+
+      this.addUser({
+        firstName: 'David',
+        lastName: 'Jackson'
+      });
+    }, 2000);
   }
 
   addUser(user: User){
