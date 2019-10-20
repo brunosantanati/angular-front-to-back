6d5455ff4098bfb5ee49776a5602cb94c61dae16
Bruno Rafael Sant' Ana <bruno.fullstackdeveloper@gmail.com>
2019-10-20 05:31:50 -0300

    Add loop in the page with ngFor

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from '../../models/User'
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
@@ -7,9 +9,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
+  users: User[];
+
   constructor() { }
 
   ngOnInit() {
+    this.users = [
+      {
+        firstName: 'Bruno',
+        lastName: 'Sant\' Ana',
+        age: 33,
+        address: {
+            street: '50 Main st',
+            city: 'Boston',
+            state: 'MA'
+        }
+      },
+      {
+        firstName: 'Kevin',
+        lastName: 'Johnson',
+        age: 34,
+        address: {
+            street: '20 School st',
+            city: 'Lynn',
+            state: 'MA'
+        }
+      },
+      {
+        firstName: 'Karen',
+        lastName: 'Williams',
+        age: 26,
+        address: {
+            street: '55 Mill st',
+            city: 'Miami',
+            state: 'FL'
+        }
+      }
+    ];
+
+    this.addUser({
+      firstName: 'David',
+      lastName: 'Jackson',
+      age: 44,
+      address: {
+          street: '12 Wake st',
+          city: 'Miami',
+          state: 'FL'
+      }
+    });
+  }
+
+  addUser(user: User){
+    this.users.push(user)
   }
 
 }
