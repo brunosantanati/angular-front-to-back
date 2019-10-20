9b31dc5d80bcf946ebee268df2788faf275e881a
Bruno Rafael Sant' Ana <bruno.fullstackdeveloper@gmail.com>
2019-10-20 03:51:08 -0300

    Add types using TypeScript

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -14,23 +14,51 @@ import { Component } from '@angular/core';
 
 export class UserComponent {
   // Properties
-  firstName = 'Bruno';
-  lastName = 'Sant\' Ana';
-  age = 32;
-  address = {
-    street: '50 Main st',
-    city: 'Boston',
-    state: 'MA'
-  };
+  firstName: string;
+  lastName: string;
+  age: number;
+  address;
+
+  foo: any;
+  hasKids: boolean;
+  numberArray: number[];
+  stringArray: string[];
+  mixedArray: any[];
+  myTuple: [string, number, boolean];
+  unusable: void;
+  u: undefined;
+  n: null;
 
   // Methods
   constructor() {
-    
-  }
+    this.firstName = 'John';
+    this.lastName = 'Doe';
+    this.age = 31;
+
+    this.address = {
+      street: '50 Main st',
+      city: 'Boston',
+      state: 'MA'
+    }
+
+    this.foo = true;
+    this.hasKids = true;
+    this.numberArray = [1,2,3];
+    this.stringArray = ['hello', 'world'];
+    this.mixedArray = [true, undefined, 'hello'];
+    this.myTuple = ['hello', 1, true];
+    this.unusable = undefined;
+    this.u = undefined;
+    this.n = null;
+
+    console.log(this.addNumbers(2, 3));
+  } 
 
   showAge() {
     return this.age + 2;
   }
 
-
+  addNumbers(num1: number, num2: number): number {
+    return num1 + num2;
+  }
 }
