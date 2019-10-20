afb59447c372000f7d9f0bb98369e42f62e5c7e8
Bruno Rafael Sant' Ana <bruno.fullstackdeveloper@gmail.com>
2019-10-20 03:36:33 -0300

    Add some methods and properties

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -13,5 +13,25 @@ import { Component } from '@angular/core';
 })
 
 export class UserComponent {
+  // Properties
+  firstName = 'John';
+  lastName = 'Doe';
+  age = 30;
+
+  // Methods
+  constructor() {
+    this.sayHello();
+    console.log(this.age);
+    this.hasBirthday();
+    console.log(this.age);
+  }
+
+  sayHello() {
+    console.log(`Hello ${this.firstName}`);
+  }
+
+  hasBirthday() {
+    this.age += 1;
+  }
 
 }
