001b2d3efcd5763b493aca2d69ce21e80c8a6ee2
Bruno Rafael Sant' Ana <bruno.fullstackdeveloper@gmail.com>
2019-10-20 05:07:59 -0300

    Generate one more component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserComponent } from './components/user/user.component'
+import { UserComponent } from './components/user/user.component';
+import { UsersComponent } from './components/users/users.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
