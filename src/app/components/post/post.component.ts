49d7e4b6ca2d5c7f52007d79b269874e211f2ccd
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-11-08 01:42:10 -0300

    Remove unnecessary dependency.

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router'
-import { Location } from '@angular/common'
 import { PostService } from '../../services/post.service'
 import { Post } from '../../models/Post'
 
@@ -15,8 +14,7 @@ export class PostComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private postService: PostService,
-    private location: Location
+    private postService: PostService
   ) { }
 
   ngOnInit() {
