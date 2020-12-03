c28b94e535b4049c0a5b139c76eb5eba3c86ba0f
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-12-02 21:18:17 -0300

    Add log list in the logs.component.ts.

--- devlogger/src/app/components/logs/logs.component.ts
+++ devlogger/src/app/components/logs/logs.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogsComponent implements OnInit {
 
+  logs: {
+    id: string,
+    text: string,
+    date: any
+  }[];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.logs = [
+      {id: '1', text: 'Generated components', date: new Date('12/26/2017 12:54:23')},
+      {id: '2', text: 'Added Bootstrap', date: new Date('12/27/2017 9:33:13')},
+      {id: '3', text: 'Added logs component', date: new Date('12/27/2017 12:00:23')}
+    ]
   }
 
 }
