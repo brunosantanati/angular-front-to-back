a7b0a638381877a6caaba9d28817caa08ea05e39
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-01-11 16:45:08 -0300

    Add, Update and Delete Logs.

--- devlogger/src/app/components/log-form/log-form.component.ts
+++ devlogger/src/app/components/log-form/log-form.component.ts
@@ -13,16 +13,49 @@ export class LogFormComponent implements OnInit {
   text: string;
   date: any;
 
+  isNew: boolean = true
+
   constructor(private logService: LogService) { }
 
   ngOnInit(): void {
     // Subscribe to the selectedLog observable
     this.logService.selectedLog.subscribe(log => {
       if(log.id !== null){
+        this.isNew = false
         this.id = log.id;
         this.text = log.text;
         this.date = log.date;
       }
+    });
+  }
+
+  onSubmit() {
+    // Check if new log
+    if(this.isNew){
+      // Create a new log
+      const newLog = {
+        id: this.generateId(),
+        text: this.text,
+        date: new Date()
+      }
+      // Add log
+      this.logService.addLog(newLog);
+    }else{
+      // Create log to be updated
+      const updLog = {
+        id: this.id,
+        text: this.text,
+        date: new Date()
+      }
+      // Update log
+      this.logService.updateLog(updLog);
+    }
+  }
+
+  generateId() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
     });
   }
 
