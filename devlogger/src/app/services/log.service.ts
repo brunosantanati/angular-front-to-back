ed751f60389d7af9d7ac8037aa55b4d96244f5b1
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-01-11 18:37:26 -0300

    Implemented Local Storage.

--- devlogger/src/app/services/log.service.ts
+++ devlogger/src/app/services/log.service.ts
@@ -28,7 +28,15 @@ export class LogService {
   }
 
   getLogs(): Observable<Log[]>{
-    return of(this.logs);
+    if(localStorage.getItem('logs') === null){
+      this.logs = [];
+    }else{
+      this.logs = JSON.parse(localStorage.getItem('logs'));
+    }
+
+    return of(this.logs.sort((a, b) => {
+      return b.date = a.date;
+    }));
   }
 
   setFormLog(log: Log){
@@ -37,6 +45,9 @@ export class LogService {
 
   addLog(log: Log){
     this.logs.unshift(log);
+
+    // Add to local storage
+    localStorage.setItem('logs', JSON.stringify(this.logs));
   }
 
   updateLog(log: Log){
@@ -46,6 +57,9 @@ export class LogService {
       }
     });
     this.logs.unshift(log);
+
+    // Update local storage
+    localStorage.setItem('logs', JSON.stringify(this.logs));
   }
 
   deleteLog(log: Log){
@@ -54,6 +68,9 @@ export class LogService {
         this.logs.splice(index, 1);
       }
     });
+
+    // Delete from local storage
+    localStorage.setItem('logs', JSON.stringify(this.logs));
   }
 
   clearState(){
