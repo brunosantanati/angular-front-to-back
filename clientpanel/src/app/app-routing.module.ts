ba6692ede0a5a6fe454792c7225bdc0a2228d408
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-01-13 10:28:56 -0300

    Routing Module Setup.

--- clientpanel/src/app/app-routing.module.ts
+++ clientpanel/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './components/dashboard/dashboard.component'
+import { LoginComponent } from './components/login/login.component'
+import { RegisterComponent } from './components/register/register.component'
+import { AddClientComponent } from './components/add-client/add-client.component'
+import { EditClientComponent } from './components/edit-client/edit-client.component'
+import { ClientDetailsComponent } from './components/client-details/client-details.component'
+import { SettingsComponent } from './components/settings/settings.component'
+import { NotFoundComponent } from './components/not-found/not-found.component'
+
+const routes: Routes = [
+  {path: '', component: DashboardComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'client/add', component: AddClientComponent},
+  {path: 'client/edit/:id', component: EditClientComponent},
+  {path: 'client/:id', component: ClientDetailsComponent},
+  {path: 'settings', component: SettingsComponent},
+  {path: '**', component: NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
