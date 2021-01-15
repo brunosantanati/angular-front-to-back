cd1f746bd5bad40a00ae7d5cf2c8ac309f21f961
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-01-15 14:50:19 -0300

    Register Guard and hide links in the navbar.

--- clientpanel/src/app/app-routing.module.ts
+++ clientpanel/src/app/app-routing.module.ts
@@ -10,11 +10,12 @@ import { ClientDetailsComponent } from './components/client-details/client-detai
 import { SettingsComponent } from './components/settings/settings.component'
 import { NotFoundComponent } from './components/not-found/not-found.component'
 import { AuthGuard } from './guards/auth.guard';
+import { RegisterGuard } from './guards/register.guard';
 
 const routes: Routes = [
   {path: '', component: DashboardComponent, canActivate:[AuthGuard]},
   {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
+  {path: 'register', component: RegisterComponent, canActivate:[RegisterGuard]},
   {path: 'client/add', component: AddClientComponent, canActivate:[AuthGuard]},
   {path: 'client/edit/:id', component: EditClientComponent, canActivate:[AuthGuard]},
   {path: 'client/:id', component: ClientDetailsComponent, canActivate:[AuthGuard]},
@@ -25,6 +26,6 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  providers: [AuthGuard]
+  providers: [AuthGuard, RegisterGuard]
 })
 export class AppRoutingModule { }
